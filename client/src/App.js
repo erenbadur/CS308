aab8d3d7be8278fe20c18331b33d307a6fa811bb
Ezgi Ergüney <ezgi.erguney@sabanciuniv.edu>
2024-11-27 09:23:53 +0300

    changed app.js to original

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
 
 // App.js
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './mainpage.css'; // Import your CSS
-import './PurchasePage.css';
-import './LoginPage.css';
-import './SignInPage.css';
 import MainPage from './mainpage';
-import PurchasePage from './PurchasePage';
-import LoginPage from './LoginPage';
-import SignInPage from './SignInPage';
+
 
 
 function App() {
     return (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={ <MainPage/> } />
-            <Route path="checkout" element={ <PurchasePage/> } />
-            <Route path="login" element={ <LoginPage/> } />
-            <Route path="signin" element={ <SignInPage/> } />
-          </Routes>
-        </BrowserRouter>
-      )
+        <div className="App">
+            <MainPage />
+        </div>
+      );
 }
 
 export default App;

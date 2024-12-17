d41e7db9de9ce2527cf0a9632472bac3de4d4ba8
Korhan Erdoğdu (Student) <korhan.erdogdu@sabanciuniv.edu>
2024-12-17 18:56:52 +0300

    Component logic created and login components implemented.

---
+++ client/src/pages/LoginPage/components/InfoSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const InfoSection = () => (
+  <div className="info-section">
+    <h1>Welcome to the Online Store 308</h1>
+    <p>
+      Shop your favorite products with ease and convenience. We bring the best
+      online trading experience right to your doorstep.
+    </p>
+    <ul>
+      <li>Exclusive deals and offers</li>
+      <li>Safe and secure payments</li>
+      <li>Fast delivery</li>
+      <li>24/7 customer support</li>
+    </ul>
+  </div>
+);
+
+export default InfoSection;
